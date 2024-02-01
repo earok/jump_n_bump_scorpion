70a7ceb02fa178d99d97ef5e77af0ee39e31008d
Erik Hogan <earok3d@gmail.com>
2024-02-02 09:43:43 +1300

    Initial check in

---
+++ JumpNBump/Template.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<tileset xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" name="templates" tilewidth="256" tileheight="256" tilecount="256" columns="0">
+  <image source="templates.png" width="256" height="256" />
+  <tile id="0">
+    <properties>
+      <property name="Name" value="p1" type="string" />
+    </properties>
+  </tile>
+  <tile id="1" />
+  <tile id="2" />
+  <tile id="3" />
+  <tile id="4" />
+  <tile id="5" />
+  <tile id="6" />
+  <tile id="7" />
+  <tile id="8" />
+  <tile id="9" />
+  <tile id="10" />
+  <tile id="11" />
+  <tile id="12" />
+  <tile id="13" />
+  <tile id="14" />
+  <tile id="15" />
+  <tile id="16">
+    <properties>
+      <property name="Name" value="spring" type="string" />
+    </properties>
+  </tile>
+  <tile id="17">
+    <properties>
+      <property name="Name" value="p1_water" type="string" />
+    </properties>
+  </tile>
+  <tile id="18">
+    <properties>
+      <property name="Name" value="splash" type="string" />
+    </properties>
+  </tile>
+</tileset>
